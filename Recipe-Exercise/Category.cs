d46315cf167f44f7187a623841b401d2c3b833c2
Khaled Ibrahim <khalidzohair12@gmail.com>
2022-07-05 12:59:08 +0200

    First implementation of TUI

---
+++ Recipe-Exercise/Category.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace Exercise_1
+{
+    // This class should serve as a singleton instance of all categories for validation.
+    // TODO: implement as a singleton class.
+    public class Category
+    {
+        public Dictionary<string, bool> Categories { get; set; }
+
+        public Category(Dictionary<string, bool> categories)
+        {
+            Categories = categories;
+        }
+
+        public bool AddCategory(string category)
+        {
+            ArgumentNullException.ThrowIfNull(category);
+
+            if (!Categories.ContainsKey(category))
+            {
+                Categories.Add(category, true);
+                return true;
+            }
+            else return false;
+        }
+
+        // TODO: Edit recipe references from old category to new category.
+        public bool EditCategory(string oldCategoryName, string newCategoryName)
+        {
+            ArgumentNullException.ThrowIfNull(oldCategoryName);
+            ArgumentNullException.ThrowIfNull(newCategoryName);
+
+            if (!Categories.ContainsKey(oldCategoryName))
+                return false;
+
+            Categories.Remove(oldCategoryName);
+            Categories.Add(newCategoryName, true);
+
+            return true;
+        }
+        
+        // TODO: Decide how to manage removed category recipes.
+        public bool RemoveCategory(string categoryName)
+        {
+            ArgumentNullException.ThrowIfNull(categoryName);
+
+            if (!Categories.ContainsKey(categoryName))
+                return false;
+
+            return Categories.Remove(categoryName);
+        }
+    }
+}
---
+++ Recipe-Exercise/Manipulator.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Exercise_1
+{
+    // This class is for containg recipes and cateogories as well serializing and deserializing them.
+    public class Manipulator
+    {
+        public List<Recipe> Recipes { get; set; }
+        public Category Categories { get; set; }
+        [JsonIgnore]
+        private readonly string _recipePath;
+        private readonly string _categoryPath;
+
+
+
+        public Manipulator()
+        {
+            Recipes = new List<Recipe>();
+            Dictionary<string, bool> catDict = new();
+            Categories = new Category(catDict);
+            _recipePath = "Recipe.json";
+            _categoryPath = "Category.json";
+
+            if (File.Exists(_categoryPath))
+            {
+                using (StreamReader read = new StreamReader(_categoryPath))
+                {
+                    string file = read.ReadToEnd();
+                    var jsonFile = JsonSerializer.Deserialize<Category>(file);
+
+                    ArgumentNullException.ThrowIfNull(jsonFile);
+
+                    Categories = jsonFile;
+                }
+            }
+
+            if (File.Exists(_recipePath))
+            {
+                using (StreamReader read = new StreamReader(_recipePath))
+                {
+                    string file = read.ReadToEnd();
+                    var jsonFile = JsonSerializer.Deserialize<List<Recipe>>(file);
+
+                    ArgumentNullException.ThrowIfNull(jsonFile);
+
+                    Recipes = jsonFile;
+                }
+            }
+
+        }
+
+        public void AddRecipe(Recipe recipe)
+        {
+            ArgumentNullException.ThrowIfNull(recipe);
+            Recipes.Add(recipe);
+        }
+
+        public void RecipeSeralize()
+        {
+            string jsonString = JsonSerializer.Serialize(Recipes);
+            File.WriteAllText(_recipePath, jsonString);
+        }
+
+        public void CategorySerialize()
+        {
+            string jsonString = JsonSerializer.Serialize(Categories);
+            File.WriteAllText(_categoryPath, jsonString);
+        }
+    }
+}
